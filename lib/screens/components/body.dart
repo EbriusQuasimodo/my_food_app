e360a03b3ff62564533fed3f38637f1d73c3ea3c
EbriusQuasimodo <sdohnisukaloveu@gmail.com>
2022-07-15 12:42:48 +0500

    first commit

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -14,16 +14,18 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        SearchBox(
-          onChanged: (value) {}, //поиск, файл search_box.dart
-        ),
-        CategoryList(),
-        ItemList(),
-        DiscountCard(),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SearchBox(
+            onChanged: (value) {}, //поиск, файл search_box.dart
+          ),
+          CategoryList(),
+          ItemList(),
+          DiscountCard(),
+        ],
+      ),
     );
   }
 }
